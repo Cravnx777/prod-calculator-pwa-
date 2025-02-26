f832383ac1bce923d15cfa6a1be8020616953d51
Cravnx777 <petruswirantositorus@gmail.com>
2025-02-26 20:21:13 +0700

    Update db.js

--- js/db.js
+++ js/db.js
@@ -1,5 +1,7 @@
 import sqlite3 from 'sqlite3';
 
+const sqlite3 = require('sqlite3').verbose();
+
 // Create a new database or open an existing one
 const db = new sqlite3.Database('calculations.db', (err) => {
     if (err) {
@@ -30,15 +32,17 @@ db.serialize(() => {
 });
 
 // Function to insert a new calculation
-export const insertCalculation = (data) => {
+export const insertCalculation = (data, callback) => {
     const { vehicleType, wke, st, lt, ct, n, s, v } = data;
-    db.run(`INSERT INTO calculations (vehicleType, wke, st, lt, ct, n, s, v) VALUES (?, ?, ?, ?, ?, ?, ?, ?)`, 
-    [vehicleType, wke, st, lt, ct, n, s, v], 
+    db.run(`INSERT INTO calculations (vehicleType, wke, st, lt, ct, n, s, v) VALUES (?, ?, ?, ?, ?, ?, ?, ?)`,
+    [vehicleType, wke, st, lt, ct, n, s, v],
     function(err) {
         if (err) {
             console.error('Error inserting calculation ' + err.message);
+            return callback(err);
         } else {
             console.log(`A calculation has been inserted with id ${this.lastID}`);
+            callback(null, this.lastID);
         }
     });
 };
@@ -48,7 +52,7 @@ export const getCalculations = (callback) => {
     db.all(`SELECT * FROM calculations`, [], (err, rows) => {
         if (err) {
             console.error('Error fetching calculations ' + err.message);
-            callback(err, null);
+            return callback(err, null);
         } else {
             callback(null, rows);
         }
@@ -56,26 +60,30 @@ export const getCalculations = (callback) => {
 };
 
 // Function to update a calculation
-export const updateCalculation = (id, data) => {
+export const updateCalculation = (id, data, callback) => {
     const { vehicleType, wke, st, lt, ct, n, s, v } = data;
-    db.run(`UPDATE calculations SET vehicleType = ?, wke = ?, st = ?, lt = ?, ct = ?, n = ?, s = ?, v = ? WHERE id = ?`, 
-    [vehicleType, wke, st, lt, ct, n, s, v, id], 
+    db.run(`UPDATE calculations SET vehicleType = ?, wke = ?, st = ?, lt = ?, ct = ?, n = ?, s = ?, v = ? WHERE id = ?`,
+    [vehicleType, wke, st, lt, ct, n, s, v, id],
     function(err) {
         if (err) {
             console.error('Error updating calculation ' + err.message);
+            return callback(err);
         } else {
             console.log(`Calculation with id ${id} has been updated.`);
+            callback(null);
         }
     });
 };
 
 // Function to delete a calculation
-export const deleteCalculation = (id) => {
+export const deleteCalculation = (id, callback) => {
     db.run(`DELETE FROM calculations WHERE id = ?`, id, function(err) {
         if (err) {
             console.error('Error deleting calculation ' + err.message);
+            return callback(err);
         } else {
             console.log(`Calculation with id ${id} has been deleted.`);
+            callback(null);
         }
     });
 };
